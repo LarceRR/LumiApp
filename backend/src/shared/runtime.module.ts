9ae997fb586d1dd22ce68e3765de2ea1c22ed5eb
LarceRR <larcerrtc@gmail.com>
2026-08-09 13:13:08 +0300

    feat: provide idempotency service globally (#28)

--- backend/src/shared/runtime.module.ts
+++ backend/src/shared/runtime.module.ts
@@ -1,5 +1,6 @@
 import { Global, Module } from '@nestjs/common';
 
+import { IdempotencyService } from './idempotency/idempotency.service';
 import { CLOCK, systemClock } from './utils/clock';
 import { ID_GENERATOR, uuidGenerator } from './utils/id';
 import { cryptoRandomSource, RANDOM_SOURCE } from './utils/random';
@@ -11,7 +12,8 @@ import { cryptoRandomSource, RANDOM_SOURCE } from './utils/random';
     { provide: CLOCK, useValue: systemClock },
     { provide: ID_GENERATOR, useValue: uuidGenerator },
     { provide: RANDOM_SOURCE, useValue: cryptoRandomSource },
+    IdempotencyService,
   ],
-  exports: [CLOCK, ID_GENERATOR, RANDOM_SOURCE],
+  exports: [CLOCK, ID_GENERATOR, RANDOM_SOURCE, IdempotencyService],
 })
 export class RuntimeModule {}
