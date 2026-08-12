e109d572a26f54f92ba01aabce62aadad5855f80
arce <larcerrtc@gmail.com>
2026-08-12 18:02:01 +0300

    feat: remove current sheet height

--- src/design-system/components/BottomSheet/BottomSheet.tsx
+++ src/design-system/components/BottomSheet/BottomSheet.tsx
@@ -93,7 +93,6 @@ export const BottomSheet = forwardRef<BottomSheetRef, BottomSheetProps>(
               alignItems: "stretch",
             }}
           >
-            <Text style={{ color: "red" }}>{sheetHeight}</Text>
             {children}
           </View>
         </KeyboardAvoidingView>
